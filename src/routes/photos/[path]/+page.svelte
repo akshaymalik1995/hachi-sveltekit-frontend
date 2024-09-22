dd12b0e6ca4f073cd338fa38aff9cad0777df33a
akshaymalik1995 <akshaymalik191@gmail.com>
2024-09-22 14:53:09 +0530

    Corrected photos view

--- src/routes/photos/[path]/+page.svelte
+++ src/routes/photos/[path]/+page.svelte
@@ -1,28 +1,14 @@
 <script>
-    import Photos from "$lib/photos.svelte";
-    import Pagination from "$lib/Pagination.svelte"
-    import {argSort} from "$lib/utils"
-    import {goto} from "$app/navigation"
+    import { DOMAIN } from "$lib/stores";
     import { page } from '$app/stores';
     export let data
-    const directories_data = data.directories_data
-    const dir = decodeURIComponent($page.params.path) 
-    const imageData = directories_data[dir]
-    imageData['sortedScoreIndex'] = argSort(imageData.list_score)
-    const imagesOnAPage = 50;
-    let currentPage = 1;
-    let totalPages = Math.ceil(imageData.list_dataHash.length / imagesOnAPage);
-    let lowerIndex = (currentPage - 1) * imagesOnAPage;
-    let upperIndex = imagesOnAPage * currentPage;
-    console.log(imageData)
-    $: {
-        currentPage = Number($page.url.searchParams.get("page")) || 1;
-        lowerIndex = (currentPage - 1) * imagesOnAPage;
-        upperIndex = imagesOnAPage * currentPage;
-    }
+    const {images_data} = data
+    const imagepath= decodeURIComponent($page.params.path) 
+    const imagedata = images_data[imagepath]
 </script>
 
-<button on:click={e => goto('/photos')} class=' mx-12 inline-block my-4 px-4 py-2 bg-blue-400 cursor-pointer hover:bg-blue-300 rounded' href="/photos">Back</button>
-
-<Photos imageData={imageData} pageSize = {{lowerIndex : lowerIndex, upperIndex : upperIndex}} />
-<Pagination {totalPages} {currentPage} />
+<!-- svelte-ignore missing-declaration -->
+<!-- svelte-ignore a11y-img-redundant-alt -->
+<div class="w-full h-screen flex justify-center items-center h-full">
+    <img class="w-auto h-screen object-cover rounded-lg shadow-xl cursor-pointer" src={DOMAIN + "/getRawDataFull/" + imagedata.hash} alt="image" />
+  </div>
