15928753dceee1498d14451db6ce7ee0ff059ede
akshaymalik1995 <akshaymalik191@gmail.com>
2024-09-22 09:25:28 +0530

    Removed photo name from gallary

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -15,14 +15,14 @@
 
     <div class='grid md:grid-cols-3 xl:grid-cols-6 gap-8'>
         {#each directories as d}
-        <a href="{'/photos/' + encodeURIComponent(d) }" class='flex gap-2 flex-col  bg-blue-300 cursor-pointer hover:bg-blue-200 py-4 px-4 rounded'>
+        <a href="{'/photos/' + encodeURIComponent(d) }" class='flex gap-2 flex-col cursor-pointer hover:bg-blue-200 py-4 px-4 rounded'>
             <!-- svelte-ignore a11y-img-redundant-alt -->
             <img
             class="h-48 rounded-lg shadow-xl cursor-pointer"
             src={DOMAIN + "/getRawData/" + directories_data[d].list_dataHash[0]}
             alt="image"
           />
-          <div class="text-center">{d}</div>
+          <!-- <div class="text-center">{d}</div> -->
         </a>
        
         {/each}
