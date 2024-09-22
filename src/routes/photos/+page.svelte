cb0690f9a1e2613699e1ddb31d3a72cf05d677c2
akshaymalik1995 <akshaymalik191@gmail.com>
2024-09-22 16:34:04 +0530

    Modal to show image completed

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -9,17 +9,18 @@
     let selectedDirectory = '';
     let filteredImages = [];
     let currentPage = 1;
-    let itemsPerPage = 50;
+    let itemsPerPage = 100;
     let imageModal = false
     let totalPages = Math.ceil(imagespath.length / itemsPerPage);
 
     function paginate() {
+       const total = 10
     scrollToTop()
-    if (totalPages <= 5) {
+    if (totalPages <= total) {
         return Array.from({ length: totalPages }, (_, i) => i + 1);
         } else {
-            const start = Math.max(1, Math.floor((currentPage - 1) / 5) * 5 + 1);
-            const end = Math.min(totalPages, start + 4);
+            const start = Math.max(1, Math.floor((currentPage - 1) / total) * total + 1);
+            const end = Math.min(totalPages, start + total - 1);
             return Array.from({ length: end - start + 1 }, (_, i) => start + i);
         }
     }
@@ -91,38 +92,75 @@
         
     }
 
-    function onImageModalClick(hash){
-        imageModal = true
-        modalimagehash = hash
+    function loadPrevImage(index){
+        modalImageIndex = index
+        let modalimagepath = getPaginatedItems(currentPage, selectedDirectory)[index]
+        modalimage = images_data[modalimagepath]
+        if (!modalimage) {
+            prevPage()
+            loadPrevImage(0)
+        }
+        
     }
 
-    let modalimagehash = ""
+    function loadNextImage(index){
+        modalImageIndex = index
+        let modalimagepath = getPaginatedItems(currentPage, selectedDirectory)[index]
+        modalimage = images_data[modalimagepath]
+        if (!modalimage) {
+            nextPage()
+            loadNextImage(0)
+        }
+        
+    }
+
+    function onImageModalClick(index){
+        modalImageIndex = index
+        imageModal = true
+        let modalimagepath = getPaginatedItems(currentPage, selectedDirectory)[index]
+        modalimage = images_data[modalimagepath]
+    }
+
+    let showFullImage = true
+
+    let modalimage = null
+    let modalImageIndex = null
 </script>
 
-<div>
 
-    <Modal class="h-screen" title="Image" size="xl" bind:open={imageModal} autoclose>
+<div class="flex justify-center items-center">
+    <Modal dialogClass="fixed top-0 start-0 end-0 h-modal md:inset-0 md:h-full z-50 w-full flex" bodyClass='bg-white h-full' headerClass='hidden' autoclose outsideclose class="h-screen" title="Image" size="xl" bind:open={imageModal}>
         <div class='w-full h-full items-center flex justify-center'>
             <!-- svelte-ignore a11y-img-redundant-alt -->
-            <img class="w-auto h-4/6 rounded-lg shadow-xl cursor-pointer" src={DOMAIN + "/getRawData/" + modalimagehash} alt="image" />
+            <img class="w-auto h-full shadow-xl cursor-pointer" src={DOMAIN + "/getRawDataFull/" + modalimage.hash} alt="image" />
             
             <!-- Add navigation buttons -->
-            <button class="absolute top-1/2 left-6 flex justify-center items-center z-10 -mt-5 w-10 h-10 bg-gray-800 text-white rounded-full focus:outline-none" onclick={() => onPreviousClick()}>
+            <button class="absolute top-1/2 left-6 flex justify-center items-center z-10 -mt-5 w-10 h-10 bg-gray-800 text-white rounded-full focus:outline-none" on:click={() => loadPrevImage(modalImageIndex - 1)}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
                 </svg>
             </button>
     
-            <button class="absolute top-1/2 right-6 z-10 flex justify-center items-center -mt-5 w-10 h-10 bg-gray-800 text-white rounded-full focus:outline-none" onclick={() => onNextClick()}>
+            <button class="absolute top-1/2 right-6 z-10 flex justify-center items-center -mt-5 w-10 h-10 bg-gray-800 text-white rounded-full focus:outline-none" on:click={() => loadNextImage(modalImageIndex + 1)}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
                 </svg>
             </button>
         </div>
     </Modal>
+</div>
+
+<div class="px-4" >
+
+    
+
+
    
-    <div class="text-center my-8 text-6xl">Gallery</div>
+    <div class="text-center  my-8 text-6xl">Gallery</div>
     <!-- I want to display directories as folders -->
+  
+   
+    
 
     <div class="directory-selector mb-8">
         <select bind:value={selectedDirectory} on:change={() => filterImagesByDirectory(selectedDirectory)} class="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
@@ -133,14 +171,14 @@
         </select>
     </div>
 
-    <div class='grid md:grid-cols-3 lg:grid-cols-4 gap-4'>
-        {#each getPaginatedItems(currentPage, selectedDirectory) as image}
+    <div class='grid md:grid-cols-3 lg:grid-cols-5 gap-4'>
+        {#each getPaginatedItems(currentPage, selectedDirectory) as image, index}
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <!-- svelte-ignore a11y-no-static-element-interactions -->
-        <div on:click={() => onImageModalClick(images_data[image].hash)} class="flex justify-center rounded-lg items-center" >
+        <div on:click={() => onImageModalClick(index)} class="flex justify-center rounded-lg items-center" >
             <div class='relative overflow-hidden rounded-lg shadow-md'>
                 <!-- svelte-ignore a11y-img-redundant-alt -->
-                <img class="w-full h-full rounded-lg  shadow-xl cursor-pointer" src={DOMAIN + "/getRawData/" + images_data[image].hash} alt="image" />
+                <img class="w-full h-48 rounded-lg  shadow-xl cursor-pointer" src={DOMAIN + "/getRawData/" + images_data[image].hash} alt="image" />
             </div>
         </div>
             
