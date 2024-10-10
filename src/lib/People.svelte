cd395b99b2433fc8c552c0357178182eab4fc55a
akshaymalik1995 <akshaymalik191@gmail.com>
2024-10-10 10:57:56 +0530

    People page ui corrected

--- src/lib/People.svelte
+++ src/lib/People.svelte
@@ -1,26 +1,55 @@
 <script>
-    import {DOMAIN} from "$lib/stores"
-    export let people_list
+  import { DOMAIN } from "$lib/stores";
+  import InfiniteScroll from "$lib/InfiniteScroll.svelte";
+  export let people_list;
+  console.log("PEOPLE LIST 2", people_list);
+  $: {
+    imagesloadedcount = 20;
+  }
+  let imagesloadedcount = 20;
+  function loadMoreImages() {
+    imagesloadedcount += 20;
+  }
 </script>
 
 <!-- Display each value in group as a clickable div, later on click we would use the PHOTOS component -->
-<div class="grid grid-cols-5 justify-center items-center gap-2 mb-16  py-2 flex-wrap">
-    <!-- A single div element -->
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
-    {#each people_list as person}
-    <!-- person => ['person_id', {}] -->
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <div class="flex flex-col justify-center items-center cursor-pointer" on:click={e => {console.log("Person id is", person[0])}}>
-                <div>
-                    <a href="{'/search?person=' + person[0]}" class="flex items-center w-48 min-h-[200px] rounded bg-gray-800">
-                        <img loading="lazy" src={DOMAIN + "/getPreviewPerson/" + person[0]} class="object-strech  hover:rounded-none rounded  w-full h-full bg-gray-100 border-gray-100 shadow-smr" alt="">
-                    </a>
-                    <div class="flex text-black my-2 text-md justify-center items-center">
-                        {person[0].slice(0, 1).toUpperCase() + person[0].slice(1).toLowerCase()}
-                    </div>
-                </div>
-
+<div
+  class="grid grid-cols-5 justify-center items-center gap-2 mb-16 py-2 flex-wrap"
+>
+  <!-- A single div element -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <InfiniteScroll loadMoreFunction={loadMoreImages} threshold={100}>
+    {#each people_list.slice(0, imagesloadedcount) as id}
+      <!-- person => ['person_id', {}] -->
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      {#if id !== "no person detected"}
+        <div
+          class="flex flex-col justify-center items-center cursor-pointer"
+          on:click={(e) => {
+            console.log("Person id is", id);
+          }}
+        >
+          <div>
+            <a
+              href={"/search?person=" + id}
+              class="flex items-center w-48 h-48 rounded bg-gray-800"
+            >
+              <img
+                loading="lazy"
+                src={DOMAIN + "/getPreviewPerson/" + id}
+                class="w-48 h-48 hover:rounded-none rounded w-full h-full bg-gray-100 border-gray-100 shadow-smr"
+                alt=""
+              />
+            </a>
+            <div
+              class="flex text-black my-2 text-md text-white justify-center items-center"
+            >
+              {id.slice(0, 1).toUpperCase() + id.slice(1).toLowerCase()}
+            </div>
+          </div>
         </div>
+      {/if}
     {/each}
+  </InfiniteScroll>
 </div>
--- src/routes/people/+page.svelte
+++ src/routes/people/+page.svelte
@@ -1,18 +1,11 @@
 <script>
     import People from "$lib/People.svelte";
-    import { page } from '$app/stores';
     export let data
     const {people_list, people_data} = data
+    console.log("PEOPLE LIST", people_list, people_data)
     const peopleDataKeyValue = Object.entries(people_data)
     peopleDataKeyValue.sort((a, b) => b[1].numOfPhotos - a[1].numOfPhotos)
-    
-    const imagesOnAPage = 25
-    let currentPage = 1
-    let totalPages = Math.ceil(people_list.length / imagesOnAPage)
-    let peopleListForThePage = people_list.slice((currentPage - 1) * imagesOnAPage, imagesOnAPage * currentPage)
-    $: {
-        currentPage = Number($page.url.searchParams.get('page')) || 1
-        peopleListForThePage = peopleDataKeyValue.slice((currentPage - 1) * imagesOnAPage, imagesOnAPage * currentPage)
-    }
 </script>
+
     
+<People people_list = {people_list} />
