1a8fa7489181a65999c24499602f0e7ca5a2df76
akshaymalik1995 <akshaymalik191@gmail.com>
2024-11-01 14:46:20 +0530

    album ui fixed

--- src/routes/albums/+page.svelte
+++ src/routes/albums/+page.svelte
@@ -1,37 +1,33 @@
 <script>
-    import {DOMAIN} from "$lib/stores"
-    export let data;
-    console.log("DATA", data)
-    const { directories_data } = data
-    let directories = Object.keys(directories_data);
-    directories = directories.sort((a, b) => a.length - b.length)
+  import {DOMAIN} from "$lib/stores"
+  export let data;
+  console.log("DATA", data)
+  const { directories_data } = data
+  let directories = Object.keys(directories_data);
+  directories = directories.sort((a, b) => a.length - b.length)
 </script>
 
-
- 
 <div class="px-4 py-8">
-    <div class="flex justify-center gap-3 flex-wrap">
-        {#each directories as directory}
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <!-- svelte-ignore a11y-no-static-element-interactions -->
-          <div class="flex bg-gray-800 w-64 flex-col">
-            <div class="relative overflow-hidden shadow-md">
-              <!-- svelte-ignore a11y-img-redundant-alt -->
-              <a href={`albums/${directory}`}>
-                <img
-                  loading="lazy"
-                  class="w-64 h-64 shadow-xl cursor-pointer transform transition-transform duration-500 hover:scale-110"
-                  src={DOMAIN +
-                    "/getRawData/" +
-                    directories_data[directory]["data_hash"][0]}
-                  alt="image"
-                />
-              </a>
-            </div>
-            <div class="p-2 bg-yellow-800 text-center text-bold">{directory}</div>
-          </div>
-        {/each}
+  <div class="flex justify-center gap-5 flex-wrap">
+    {#each directories as directory}
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <!-- svelte-ignore a11y-no-static-element-interactions -->
+      <div class="flex shadow-xl rounded-lg bg-gray-100 dark:bg-gray-900 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 flex-col">
+      <div class="relative rounded-t-lg overflow-hidden">
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <a href={`albums/${directory}`}>
+        <img
+          loading="lazy"
+          class="w-full h-64  cursor-pointer transform transition-transform duration-500 hover:scale-110"
+          src={DOMAIN +
+          "/getRawData/" +
+          directories_data[directory]["data_hash"][0]}
+          alt="image"
+        />
+        </a>
       </div>
-
+      <div class="p-2 rounded-b-lg dark:bg-gray-700 dark:text-white text-center text-bold">{directory}</div>
+      </div>
+    {/each}
+    </div>
 </div>
-
--- src/routes/albums/[path]/+page.svelte
+++ src/routes/albums/[path]/+page.svelte
@@ -1,25 +1,17 @@
-
 <script>
-    import Photos from "$lib/photos.svelte";
-    import {directoriesDataStore} from "$lib/stores.js";
-    import { page } from '$app/stores';
+  import Photos from "$lib/photos.svelte";
+  import {directoriesDataStore} from "$lib/stores.js";
+  import { page } from '$app/stores';
 
-    const path = decodeURIComponent($page.params.path);
-    const imagesdata = $directoriesDataStore[path];
-
+  const path = decodeURIComponent($page.params.path);
+  const imagesdata = $directoriesDataStore[path];
 </script>
 
-
-
-<div class="">
-    <div class="flex justify-center items-center">
-        <button
-          class="bg-gray-800 hover:bg-gray-700 text-gray-500 font-bold py-2 px-4 rounded"
-          onclick="window.history.back()">Back</button
-        >
-        <div class="grow text-center">
-          <div class="text-4xl -translate-x-1/2 inline-block p-8">{path.toUpperCase()}</div>
-        </div>
-      </div>
-<Photos images_data={imagesdata} />
+<div class="dark:bg-gray-900 bg-white min-h-screen">
+  <div class="flex justify-center items-center">
+    <div class="grow text-center">
+      <div class="text-lg sm:text-xl lg:text-4xl inline-block p-2 text-black dark:text-white">{path.toUpperCase()}</div>
+    </div>
+  </div>
+  <Photos images_data={imagesdata} />
 </div>
