bee5dd5e873ecce3c86a1639513f1b28ebf83316
akshaymalik1995 <akshaymalik191@gmail.com>
2024-11-01 21:14:12 +0530

    changed domain name

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,5 +1,5 @@
 import { writable } from "svelte/store";
-export const DOMAIN = "http://localhost:8200"
+export const DOMAIN = "http://192.168.31.166:5000/api"
 
 export const MonthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"]
